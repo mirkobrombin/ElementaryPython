2564c4ab87bad463495d5a70b3114d9288408bf8
Mirko Brombin <brombinmirko@gmail.com>
2017-10-04 14:46:52 +0200

    Ops

--- elementarypython/headerbar.py
+++ elementarypython/headerbar.py
@@ -2,20 +2,20 @@
 '''
    Copyright 2017 Mirko Brombin (brombinmirko@gmail.com)
 
-   This file is part of Football.
+   This file is part of ElementaryPython.
 
-    Football is free software: you can redistribute it and/or modify
+    ElementaryPython is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Football is distributed in the hope that it will be useful,
+    ElementaryPython is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Football.  If not, see <http://www.gnu.org/licenses/>.
+    along with ElementaryPython.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import gi
