ef56dd60593df51aa392a1a6bfb6a6aa41dc46e2
Mirko Brombin <brombinmirko@gmail.com>
2017-10-04 15:25:09 +0200

    Stuff

---
+++ README.md
@@ -0,0 +1,37 @@
+# Elementary Python
+
+This is a python application template for elementary OS.
+![Screenshot](screenshot.png)
+
+## Requirements
+
+- python3
+- libgtk-3-dev
+- libgranite-dev 
+
+## How to run
+
+```bash
+com.github.mirkobrombin.elementarypython
+```
+
+## Installation
+
+### For Debian based
+
+Grab an updated debian package [here](https://github.com/mirkobrombin/ElementaryPython/releases)
+
+```bash
+dpkg -i com.github.mirkobrombin.elementarypython*.deb
+```
+
+### Install from source
+
+Grab an updated release [here](https://github.com/mirkobrombin/ElementaryPython/archive/master.zip) or use **git**:
+
+```bash
+git clone https://github.com/mirkobrombin/ElementaryPython.git
+cd ElementaryPython
+sudo python3 setup.py install
+```
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import glob, os 
+from distutils.core import setup
+
+install_data = [('share/applications', ['data/com.github.mirkobrombin.elementarypython.desktop']),
+                ('share/metainfo', ['data/com.github.mirkobrombin.elementarypython.appdata.xml']),
+                ('share/icons/hicolor/128x128/apps',['data/com.github.mirkobrombin.elementarypython.svg']),
+                ('bin/elementarypython',['elementarypython/constants.py']),
+                ('bin/elementarypython',['elementarypython/headerbar.py']),
+                ('bin/elementarypython',['elementarypython/main.py']),
+                ('bin/elementarypython',['elementarypython/welcome.py']),
+                ('bin/elementarypython',['elementarypython/window.py']),
+                ('bin/elementarypython',['elementarypython/__init__.py'])]
+
+setup(  name='Elementary Python',
+        version='0.0.1',
+        author='Mirko Brombin',
+        description='This is just a python template',
+        url='https://github.com/mirkobrombin/elementarypython',
+        license='GNU GPL3',
+        scripts=['com.github.mirkobrombin.elementarypython'],
+        packages=['elementarypython'],
+        data_files=install_data)
