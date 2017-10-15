b4dd974f4198b39b3c19a3048585b9d8fdbb377a
Mirko Brombin <brombinmirko@gmail.com>
2017-10-15 23:31:43 +0200

    Fixed NameError: name 'Gtk' is not defined

--- elementarypython/welcome.py
+++ elementarypython/welcome.py
@@ -21,6 +21,9 @@
 import constants as cn
 import subprocess
 import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('Granite', '1.0')
+from gi.repository import Gtk, Gdk, Granite
 import webbrowser
 
 
