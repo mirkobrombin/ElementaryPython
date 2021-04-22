c59a557326b89d1eb17302b2ec3b51c2581016f1
Mirko Brombin <send@mirko.pm>
2021-04-22 19:33:12 +0200

    Documenting window.py

--- elementarypython/window.py
+++ elementarypython/window.py
@@ -17,23 +17,33 @@
     You should have received a copy of the GNU General Public License
     along with ElementaryPython.  If not, see <http://www.gnu.org/licenses/>.
 '''
+import gi
+
+gi.require_version('Gtk', '3.0')
+gi.require_version('Granite', '1.0')
+
+from gi.repository import Gtk
+
 import constants as cn
 import headerbar as hb
 import welcome as wl
 
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, Gdk
-
-
 class Window(Gtk.Window):
 
     def __init__(self):
-        Gtk.Window.__init__(self, title=cn.App.application_name)
+        Gtk.Window.__init__(
+            self, 
+            title=cn.App.application_name
+        )
 
+        '''Here we are creating a new instance of Headerbar 
+        and setting as the titlebar'''
         hbar = hb.Headerbar()
         self.set_titlebar(hbar)
 
+        '''This is a new instance of Welcome, we are 
+        simply adding it to the window
+        Note: Not all applications need a Welcome Page, make 
+        your choice well.'''
         self.welcome = wl.Welcome()
-
         self.add(self.welcome)
