21c70e8a7ccc6f6bd9844e8dec0de901d5c0b4d9
Mirko Brombin <send@mirko.pm>
2021-04-22 19:59:21 +0200

    Documenting headerbar.py

--- elementarypython/headerbar.py
+++ elementarypython/headerbar.py
@@ -18,12 +18,14 @@
     along with ElementaryPython.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import constants as cn
 import gi
 import webbrowser
+
 gi.require_version('Gtk', '3.0')
+
 from gi.repository import Gtk, Gdk
 
+import constants as cn
 
 class Headerbar(Gtk.HeaderBar):
 
@@ -31,36 +33,55 @@ class Headerbar(Gtk.HeaderBar):
 
         Gtk.HeaderBar.__init__(self)
 
+        '''Here we are setting some parameters for the HeaderBar
+        <https://developer.gnome.org/gtk3/stable/GtkHeaderBar.html>'''
         self.set_show_close_button(True)
         self.props.title = cn.App.application_name
 
-        # help button
-        self.hbar_help = Gtk.ToolButton()
-        self.hbar_help.set_icon_name("help-contents")
-        self.hbar_help.connect("clicked", self.on_hbar_help_clicked)
-        self.pack_end(self.hbar_help)
+        '''Now we want to display some buttons in our HeaderBar. We are
+        using ToolButton, a gtk.ToolItem subclass that displays buttons'''
+        self.hbar_help = Gtk.ToolButton() # a new instance
+        self.hbar_help.set_icon_name( # setting the button icon name
+            "help-contents" 
+        ) 
+        self.hbar_help.connect( # connecting our method to the clicked signal
+            "clicked", 
+            self.on_hbar_help_clicked
+        )
+        self.pack_end( # packing the button to the end of the HeaderBar
+            self.hbar_help
+        )
         
-        # color button
-        self.hbar_color = Gtk.ColorButton.new_with_rgba(Gdk.RGBA(222, 222, 222, 255))
-        self.hbar_color.connect("color_set", self.on_hbar_color_color_set)
+        '''Another button, this can be used for choosing Application colors'''
+        self.hbar_color = Gtk.ColorButton.new_with_rgba(
+            Gdk.RGBA(222, 222, 222, 255)
+        )
+        self.hbar_color.connect(
+            "color_set", 
+            self.on_hbar_color_color_set
+        )
         self.pack_end(self.hbar_color)
 
+    '''Each button should perform actions, no?
+    We are creating custom methods for each button.'''
     def on_hbar_help_clicked(self, widget):
-        webbrowser.open_new_tab("https://github.com/mirkobrombin/ElementaryPython")
+        webbrowser.open_new_tab(
+            "https://github.com/mirkobrombin/ElementaryPython"
+        )
 
     def on_hbar_color_color_set(self, widget):
         cn.Colors.primary_color = widget.get_rgba().to_string()
 
-        # There are better methods to define CSS variables, but this is an example.
-        stylesheet = """
-            @define-color colorPrimary """+cn.Colors.primary_color+""";
-            @define-color textColorPrimary """+cn.Colors.primary_text_color+""";
-            @define-color textColorPrimaryShadow """+cn.Colors.primary_text_shadow_color+""";
+        stylesheet = f"""
+            @define-color colorPrimary {cn.Colors.primary_color};
+            @define-color textColorPrimary {cn.Colors.primary_text_color};
+            @define-color textColorPrimaryShadow {cn.Colors.primary_text_shadow_color};
         """
 
         style_provider = Gtk.CssProvider()
         style_provider.load_from_data(bytes(stylesheet.encode()))
         Gtk.StyleContext.add_provider_for_screen(
-            Gdk.Screen.get_default(), style_provider,
+            Gdk.Screen.get_default(), 
+            style_provider,
             Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
         )
